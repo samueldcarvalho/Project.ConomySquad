cfda5e4290abeb26b7e92ec3edb7ba641db1e2b3
Samuel de Carvalho <80074114+samueldcarvalho@users.noreply.github.com>
2021-07-15 17:34:23 -0300

    Modelagem de Entidades

---
+++ src/Api.Domain/Entities/Entity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    abstract class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        private DateTime? _dataCriacao;
+        public DateTime? DataCriacao
+        {
+            get { return _dataCriacao; }
+            set { _dataCriacao = (value == null ? DateTime.UtcNow : value); }
+        }
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
